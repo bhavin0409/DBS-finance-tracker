565ee9f16768989366b6483380927129406a7a58
devjoshi1064 <joshidev882@gmail.com>
2025-09-18 14:19:09 +0530

    user fetch half completed

---
+++ Hook/use-fetch.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+const userFetch = (cb) => {
+    const [data, setData] = useState(undefined);
+    const [loading, setLoading] = useState(null);
+    const [error, setError] = useState(null);
+    const fn = async (...args) => {
+        setLoading(true);
+        setError(null);     
+        try {
+            const response = await cb(...args);
+            setData(response);
+            setError(null);
+        } catch (err) {
+            setError(err);
+            
+        }
+    };
+    return { data, loading, error, fn ,setData};
+};
+export default userFetch; 
