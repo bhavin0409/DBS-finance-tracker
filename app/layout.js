51ac722f238e66f812b05210fbe6d4e69766caf4
Bhavin Prajapati <prajapatibhavin989@gmail.com>
2025-09-18 14:11:17 +0530

    create a account drawer done

--- app/layout.js
+++ app/layout.js
@@ -14,10 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body
-          className={`${inter.className} `}
-          
-        >
+        <body className={`${inter.className} highlighter-context`}>
           <Header />
           <main className="h-screen mx-auto py-5">
             {children}
